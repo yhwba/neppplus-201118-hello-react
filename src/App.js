0d1b0d587a76888e34270480b915ed80d5d784f1
Heawon.Y <yhwba1216@gmail.com>
2020-11-18 20:32:59 +0900

    - web : build 폴더 이름 변경해서 추가

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import logo from './logo.svg';
+// import logo from './spongebob.png';
+import bob from './original.gif';
 import './App.css';
 
 function App() {
+  var logo = "spongebob.png";
+  var text ="스폰지 밥입니다.";
+  var number = (167+6841-5665)*1155
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={bob} className="" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {text} //
         </p>
+        <span className="num">{number}</span>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://naver.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          아닌데요? 뚱인데요?
         </a>
       </header>
     </div>
